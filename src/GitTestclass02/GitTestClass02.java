8a7361df489880e84de53fd50be8ff0376cf3164
unknown <rhita@rhizlane.com>
2022-09-29 04:51:07 -0400

    simple basic project

---
+++ src/GitTestclass02/GitTestClass02.java
@@ -0,0 +1,14 @@
+package GitTestclass02;
+
+public class GitTestClass02 {
+
+	public static void main(String[] args) {
+		
+
+		for(int i=0;i<8;i++) {
+			
+			System.out.print(i+" ");
+		}
+	}
+
+}
