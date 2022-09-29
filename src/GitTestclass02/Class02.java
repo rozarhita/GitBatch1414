4fe23a42a72a6e5e0d6b419268637ab0c88583d3
unknown <rhita@rhizlane.com>
2022-09-29 13:04:08 -0400

    third projet

---
+++ src/GitTestclass02/Class02.java
@@ -0,0 +1,12 @@
+package GitTestclass02;
+
+public class Class02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("HELLO WORD");
+ System.out.println("this is a Git class");
+ System.out.println("hello");
+	}
+
+}
